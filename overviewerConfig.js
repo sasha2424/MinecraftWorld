5c610ed430493e13474968c2fc861ce6d5bbf378
alexanderivanov2424 <alexander_ivanov@brown.edu>
2021-02-06 16:36:18 -0500

    update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1611733453",
+        "cacheTag": "1612635178",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1611732177,
+            "last_rendertime": 1612634237,
             "imgextension": "jpg",
             "isOverlay": false,
             "poititle": "Markers",
